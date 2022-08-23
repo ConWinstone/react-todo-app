1c2ad969063fb2d5b3ca02a633bc9060e76bfa1b
ConWinstone <conwin@veeqo.com>
2022-08-23 16:02:19 +0100

    Updated Styling on all components

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,27 @@
 import React from 'react'
+
+//stylesheet
+import styles from './TodoItem.module.css'
+
+const completedStyle = {
+  fontStyle: 'italic',
+  color: '#595959',
+  opacity: 0.4,
+  textDecoration: 'line-through',
+}
+
 function TodoItem(props) {
   return (
-    <li>
+    <li className={styles.item}>
       <input
         type="checkbox"
+        className={styles.checkbox}
         checked={props.todo.completed}
         onChange={() => props.handleChangeProps(props.todo.id)}
       />
-      {props.todo.title}
+      <span style={props.todo.completed ? completedStyle : null}>
+        {props.todo.title}
+      </span>
       <button onClick={() => props.deleteTodoProps(props.todo.id)}>
         Delete
       </button>
