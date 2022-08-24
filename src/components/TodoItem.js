49a4edb112346181ea40d8072ad742c330f335e3
ConWinstone <conwin@veeqo.com>
2022-08-24 13:28:10 +0100

    Editing the todos

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -10,22 +10,61 @@ const completedStyle = {
   textDecoration: 'line-through',
 }
 
-function TodoItem(props) {
-  return (
-    <li className={styles.item}>
-      <input
-        type="checkbox"
-        className={styles.checkbox}
-        checked={props.todo.completed}
-        onChange={() => props.handleChangeProps(props.todo.id)}
-      />
-      <span style={props.todo.completed ? completedStyle : null}>
-        {props.todo.title}
-      </span>
-      <button onClick={() => props.deleteTodoProps(props.todo.id)}>
-        Delete
-      </button>
-    </li>
-  )
+class TodoItem extends React.Component {
+  state = {
+    editing: false,
+  }
+
+  handleEditing = () => {
+    this.setState({
+      editing: true,
+    })
+  }
+
+  handleUpdatedDone = (event) => {
+    if (event.key === 'Enter') {
+      this.setState({ editing: false })
+    }
+  }
+
+  render() {
+    let viewMode = {}
+    let editMode = {}
+
+    if (this.state.editing) {
+      viewMode.display = 'none'
+    } else {
+      editMode.display = 'none'
+    }
+
+    return (
+      <li className={styles.item}>
+        <div onDoubleClick={this.handleEditing} style={viewMode}>
+          <input
+            type="checkbox"
+            className={styles.checkbox}
+            checked={this.props.todo.completed}
+            onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+          />
+          <span style={this.props.todo.completed ? completedStyle : null}>
+            {this.props.todo.title}
+          </span>
+        </div>
+        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+          Delete
+        </button>
+        <input
+          type="text"
+          style={editMode}
+          className={styles.textInput}
+          value={this.props.todo.title}
+          onChange={(e) => {
+            this.props.setUpdate(e.target.value, this.props.todo.id)
+          }}
+          onKeyDown={this.handleUpdatedDone}
+        />
+      </li>
+    )
+  }
 }
 export default TodoItem
